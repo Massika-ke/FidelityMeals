5329193fd9dfa3a0f3192da4b47df8c181c77d36
Massika-Ke <wamwoka99@gmail.com>
2024-11-26 13:56:19 +0300

    Cart Items number update

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -10,12 +10,33 @@ const defaultCartState = {
 
 const cartReducer = (state, action)=>{
   if (action.type === 'ADD'){
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+// check if item exists in the items
+    const existingCartItemIndex = state.items.findIndex(
+      (item => item.id === action.item.id)
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+// If item already exists in the item, update
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+// Add/update item amount as new entry 
+    else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return{
       items: updatedItems,
-      totalAmount: updatedTotalAmount
-    }
+      totalAmount: updatedTotalAmount,
+    };
   }
   return defaultCartState;
 }
